a38ddc9d0a1d7bfd51fe70dcd6895a7d931a77fa
caoabunga <caoabunga@me.com>
2020-08-04 09:24:54 -0700

    updating cmds

--- dotnet-cmds.sh
+++ dotnet-cmds.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
 # command for packaging lambda function into deployment zip file
-# dotnet lambda package -c Debug -o ../HelloWorldLambdaDotNet.zip -f netcoreapp3.1
+# dotnet lambda package -c Debug -o ../aetserver.zip -f netcoreapp3.1 --msbuild-parameters "AETGatewayLambda.csproj"
 
 # command for deploying into aws
 # dotnet lambda deploy-function MySimpleFunction
 
 # command for invoking lambda function
 dotnet lambda invoke-function MySimpleFunction -–region us-east-1
+
+## installing sam cli for local lambda/api gateway testing
+
+# brew tap aws/tap
+# brew install aws-sam-cli
+
+## verify install
+# sam --version
